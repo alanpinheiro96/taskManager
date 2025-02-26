800742a0eded687497c6a18178150ff09d12ae94
AlanPinheiro96 <alantnt15@gmail.com>
2025-02-25 22:31:06 -0300

    adiciona botoes  com icones de interação ao cliente

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,17 +1,28 @@
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { Text, View, StyleSheet, ImageBackground, TouchableOpacity } from "react-native";
 
-import moment from "moment";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import moment from "moment-timezone";
 import 'moment/locale/pt-br';
 
 import todayImage from '../../assets/imgs/today.jpg'
 
 export default function TaskList() {
-    const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+    const userTimeZone = moment.tz.guess(); //detecta fuso horario do dispositivo
+    console.log(userTimeZone)
+    const today = moment().tz("America/Sao_Paulo").locale('pt-br').format('ddd, D [de] MMMM')
+    //const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
 
     return(
         <View style={styles.container}> 
 
             <ImageBackground source={todayImage} style={styles.background}>
+                <View style={styles.iconBar}>
+                    <TouchableOpacity onPress={() => console.warn('oi')}>
+                        <Icon name="eye" size={60} color={'#fff'}/>
+                    </TouchableOpacity>
+                </View>
+
                 <View style={styles.titleBar}>
                     <Text style={styles.title}>Hoje</Text>
                     <Text style={styles.subtitle}>{today}</Text>
@@ -21,6 +32,10 @@ export default function TaskList() {
             <View style={styles.taskList}>
 
             </View>
+            <TouchableOpacity style={styles.addButton}activeOpacity={0.7}onPress={() => console.warn('+')}>
+                <Icon name="plus" size={40} color={'#fff'}/>
+            </TouchableOpacity>
+
         </View>
     )
 }
@@ -52,5 +67,21 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 30
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: 20
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        borderRadius: 25, 
+        backgroundColor: '#B13B44',
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 })
